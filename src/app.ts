6869e32c6d0de540c07aeb01da09baed3ea65dd2
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2022-05-10 06:58:17 -0300

    2th commit

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get('/github', (request, response) => {
+  response.redirect(`http://github.com/login/oauth/authorize?client_id=
+  ${process.env.GITHUB_CLIENT_ID}`
+  );
+})
+
 app.listen(4000, () => console.log('Server is run on PORT 4000.'));
