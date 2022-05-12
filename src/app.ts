0f5a7988a085e81f9fdcd8508b1a33bdab83ca21
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2022-05-12 06:26:21 -0300

    3th commit

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
+import "dotenv/config";
 import express from "express";
+
+import { router } from "./services/routes";
 
 const app = express();
 
 app.get('/github', (request, response) => {
-  response.redirect(`http://github.com/login/oauth/authorize?client_id=
-  ${process.env.GITHUB_CLIENT_ID}`
+  response.redirect(`http://github.com/login/oauth/authorize?client_id=${process.env.
+  GITHUB_CLIENT_ID}`
   );
+})
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
 })
 
 app.listen(4000, () => console.log('Server is run on PORT 4000.'));
